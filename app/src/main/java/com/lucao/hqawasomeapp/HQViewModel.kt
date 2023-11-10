eb584dcab07b7008392f19bde6e1852b8838a380
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2023-11-09 22:16:40 -0300

    inicial commit arch

---
+++ app/src/main/java/com/lucao/hqawasomeapp/HQViewModel.kt
@@ -0,0 +1,7 @@
+package com.lucao.hqawasomeapp
+
+import androidx.lifecycle.ViewModel
+
+class HQViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/lucao/hqawasomeapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.lucao.hqawasomeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
